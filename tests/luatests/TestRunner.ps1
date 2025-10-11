9babf21b47eadbb51f95bec976a1f2c0f1b86fd2
TheMakarik <themakarik@gmail.com>
2025-10-11 17:02:29 +0700

    Build luaunit

--- tests/luatests/TestRunner.ps1
+++ tests/luatests/TestRunner.ps1
@@ -1,18 +1,19 @@
-Write-Host "Starting lua scripts..." -ForegroundColor Green
+Write-Host "Starting lua scripts..." -ForegroundColor Green;
 
-$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
-$luaExePath = Join-Path $scriptDirectory "lua54.exe"
-$testsPath = Join-Path $scriptDirectory "tests"
-$luaunitPath = Join-Path $scriptDirectory "luaunit"
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$luaExePath = Join-Path $scriptDirectory "lua54.exe";
+$testsPath = Join-Path $scriptDirectory "tests";
+$luaunitPath = Join-Path $scriptDirectory "luaunit";
 
-$modulesPath = Join-Path (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $scriptDirectory))) "SolidZip\src\SolidZip\modules"
-$modulesPathLua = $modulesPath -replace '\\', '/'
-$modulesSearchPath = "$modulesPathLua/?.lua;$modulesPathLua/logging/?.lua"
+$modulesPath = Join-Path (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $scriptDirectory))) "SolidZip\src\SolidZip\modules";
+$modulesPathLua = $modulesPath -replace '\\', '/';
+$modulesSearchPath = "$modulesPathLua/?.lua;$modulesPathLua/logging/?.lua";
 
-
-$luaunitPathLua = $luaunitPath -replace '\\', '/'
+$luaunitOutPath = Join-Path $luaunitPath "luaunit.out";
+$luaunitOutPathLua = $luaunitOutPath -replace '\\', '/';
 
 Get-ChildItem -Path $testsPath -Filter "*Tests.lua" | ForEach-Object {
-    Write-Host "Running $($_.Name)..." -ForegroundColor Yellow
-    & $luaExePath -e "package.path = package.path .. ';$luaunitPathLua/?.lua;$modulesSearchPath'" $_.FullName
+    Write-Host "Running $($_.Name)..." -ForegroundColor Yellow;
+    & $luaExePath -e "luaunit = dofile('$luaunitOutPathLua'); package.preload['luaunit'] = function() return luaunit end; package.path = package.path .. ';$modulesSearchPath'" $_.FullName;
 }
+exit $LASTEXITCODE;
