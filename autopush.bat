6d4491b19e7c7f8b4611585b7695384a8e2dcd23
TheMakarik <themakarik@gmail.com>
2025-12-11 16:20:31 +0300

    "

--- autopush.bat
+++ autopush.bat
@@ -9,47 +9,46 @@ SETLOCAL EnableDelayedExpansion
 
 SET "TestResult=test_output.txt"
 SET "CommitResult=commit_output.txt"
-SET "PushResult=push_output.txt"
 
 IF "%1" NEQ "" (
-    ECHO "[INFO] Start testing..."
-    dotnet test > %TestResult%
+    ECHO [INFO] Start testing...
+    dotnet test > "%TestResult%"
     IF !ERRORLEVEL! EQU 0 (
         ECHO [INFO] Tests were succeed
+        
         ECHO [INFO] Adding files to the git
         git add .
+        
         ECHO [INFO] Checking internet connection
         ping google.com -n 1 -w 2000 > nul
         IF !ERRORLEVEL! EQU 0 (
             ECHO [INFO] Making commit
-            git commit -m "%1" > %CommitResult%
+            git commit -m "%1" > "%CommitResult%"
             IF !ERRORLEVEL! EQU 0 (
                 ECHO [INFO] Commit was succeed
+                
                 ECHO [INFO] Making push
-                git push > %PushResult%
+                git push
                 if !ERRORLEVEL! EQU 0 (
                     ECHO [SUCCESS] Push was made
                 ) ELSE (
                     ECHO [ERROR] Push error
-                    ECHO Push output: %PushResult%
                 )
             ) ELSE (
                 ECHO [ERROR] Commit error
-                ECHO Commit output: %CommitResult%
+                TYPE "%CommitResult%"
             )
         ) ELSE (
             ECHO [ERROR] No internet connection
         )
     ) ELSE (
         ECHO [ERROR] Tests was not succeed
-        ECHO %TestResult%
+        TYPE "%TestResult%"
     )
-  
 ) ELSE (
     ECHO [FATAL] Commit message argument is missing
 )
 
-DEL "%TestResult%" "%CommitResult%" "%PushResult%" > nul 2>&1
-
+DEL "%TestResult%" "%CommitResult%" nul 2>&1
 PAUSE
-ENDLOCAL 
+ENDLOCAL
